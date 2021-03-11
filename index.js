4bb2054c9e59d0db94c57fe649808210bebe7b57
Thyda Korng <thydakorng@gmail.com>
2021-03-11 15:11:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function happyHolidays(String) {
 function happyHolidaysTo(String) {
   return `Happy holidays, ${name}!`
 }
+
+function happyCustomHolidayTo(String) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(string) {
+  return `It's ${days} days until ${holiday}!`
+}
